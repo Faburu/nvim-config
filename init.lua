fc788fbf877e3b22bbe73307fd6960cf0ad1d204
faburu <fablecarmona@gmail.com>
2024-01-30 22:50:21 -0500

    converted to lua

---
+++ init.lua
@@ -0,0 +1,22 @@
+vim.opt.relativenumber = true
+vim.opttabstop=4
+vim.opt.expandtab = true
+vim.opt.shiftwidth=4
+vim.opt.softtabstop=4
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+require("lazy").setup("plugins")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,4 @@
+return{
+	{"git@github.com:nvim-treesitter/nvim-treesitter.git", version = "*"},
+	{"Vigemus/iron.nvim", version = "*"}
+}
